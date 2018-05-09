ecdec302b1bfa2e81c674aa0607fea44c178a4db
Hibari-K <smiya_revaken@yahoo.co.jp>
2018-05-09 10:24:56 +0900

    Add code for memory shortage and WSL compatibility

--- compile.sh
+++ compile.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -x
+
 cd /usr/src
 su -c "grep '^deb ' /etc/apt/sources.list | sed 's/^deb/deb-src/g' > /etc/apt/sources.list.d/deb-src.list"
 sed -i'~' -E "s@http://(..\.)?(archive|security)\.ubuntu\.com/ubuntu@http://linux.yz.yamagata-u.ac.jp/pub/linux/ubuntu-archive/@g" /etc/apt/sources.list
+
+dd if=/dev/zero of=/swap bs=1M count=4096
+chmod 600 /swap
+mkswap /swap
+swapon /swap
 
 apt update -qq
 apt install -y git
@@ -20,3 +26,4 @@ make install
 popd
 sed -i -e "s/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet memmap=0x80000000\$0x80000000\"/g" /etc/default/grub
 update-grub2
+swapoff /swap
--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
 #!/bin/sh -x
+memall=`free -tg | sed -n 2P | cut -d' ' -f15`
+if [ ${memall} -lt 7 ]; then
+  sed -i -e 's/4096/1024/g' Vagrantfile
+fi
 vagrant up
 vagrant ssh -c "sudo bash -x /vagrant/compile.sh"
+if [ ${memall} -lt 7 ]; then
+  sed -i -e 's/1024/4096/g' Vagrantfile
+fi
 vagrant reload
-vbox_version=`virtualbox --help | sed -n 1P | cut -d' ' -f5`
+WINDIR="/mnt/c/Windows"
+if [ -d ${WINDIR} ]; then
+  vbox_version=`VBoxManage.exe --help | sed -n 1P | cut -d' ' -f9 | tr -d "\r"`
+else
+  vbox_version=`virtualbox --help | sed -n 1P | cut -d' ' -f5`
+fi
 vagrant ssh -c "wget https://raw.githubusercontent.com/liva/hakase_vboxenv/master/install_vboxguestadditions.sh;
                 bash -x ./install_vboxguestadditions.sh ${vbox_version};
                 rm ./install_vboxguestadditions.sh"
